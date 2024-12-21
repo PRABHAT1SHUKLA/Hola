1f341d2acfca9c41cfc5d271c873e366b66b1c23
prabhat <prabhatshukla0849@gmail.com>
2024-12-21 16:47:36 +0530

    feat:dropdown menu component  added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
