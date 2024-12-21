f6e8972e408fc220ea744d05a6e6dce20de2cb97
prabhat <prabhatshukla0849@gmail.com>
2024-12-21 17:09:53 +0530

    feat : layout changing function in meeting room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react'
+
+ 
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
+  
+  const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+  
+  const CallLayout = () =>{
+    switch(layout){
+      case 'grid':
+        return <PaginatedGridLayout />;
+      case 'speaker-right':
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  }
+
   return (
     <div>MeetingRoom</div>
   )
