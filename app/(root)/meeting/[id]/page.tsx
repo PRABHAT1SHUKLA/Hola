ea8fb9845c3c1ad43dc3a651369300c91591f0a5
prabhat <prabhatshukla0849@gmail.com>
2024-12-21 07:58:31 +0530

    feat: created dyamic route handler for meeting involving setup and room

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useGetCallById } from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation'
+import React, { useState } from 'react'
 
 const meeting = ({params}:{params:{id: string}}) => {
+   
+  const { id } = useParams();
+  if(!id) return
+  const { isLoaded, user } = useUser();
+  const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call) return (
+    <p className="text-center text-3xl font-bold text-white">
+      Call Not Found
+    </p>
+  );
+
   return (
-    <div>meeting {params.id}</div>
-  )
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+
+        {!isSetupComplete ? (
+          <MeetingSetup  />
+        ) : (
+          <MeetingRoom />
+        )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 }
 
 export default meeting
