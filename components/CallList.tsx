2abd4c8f1f5a49341dbe9a808672b4e1c0bb80e4
prabhat <prabhatshukla0849@gmail.com>
2024-12-22 17:56:05 +0530

    feat: call component for diff routes configured

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -78,9 +78,35 @@ const CallList = ({type}:{type:'ended'|'upcoming'|'recordings'}) =>{
                 ? '/icons/upcoming.svg'
                 : '/icons/recordings.svg'
           }
+          title={
+            (meeting as Call).state?.custom?.description ||
+            (meeting as CallRecording).filename?.substring(0, 20) ||
+            'No Description'
+          }
+          date={
+            (meeting as Call).state?.startsAt?.toLocaleString() ||
+            (meeting as CallRecording).start_time?.toLocaleString()
+          }
+          isPreviousMeeting={type === 'ended'}
+
+          link={
+            type === 'recordings'
+              ? (meeting as CallRecording).url
+              : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`
+          }
+
+          buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
+          buttonText={type === 'recordings' ? 'Play' : 'Start'}
+          handleClick={
+            type === 'recordings'
+              ? () => router.push(`${(meeting as CallRecording).url}`)
+              : () => router.push(`/meeting/${(meeting as Call).id}`)
+          }
 
           />
         ))
+      ) : (
+        <h1 className="text-2xl font-bold text-white">{noCallsMessage}</h1>
       )}
     </div>
   )
